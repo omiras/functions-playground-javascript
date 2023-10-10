81f7b7e1a2b9d02a477457cd3e9804f5f1a2c287
omiras <oscar.miras@gmail.com>
2023-10-10 22:36:47 +0200

    aclaraciones

--- ex2.js
+++ ex2.js
@@ -2,13 +2,13 @@
  * Invoca a la función con el valor de parámetros adecuados, para que se muestre por consola el valor "10". SOLO puedes modificar el valor de las variables sum1 y sum2.
  */
 
- function suma(a, b) {
-     return a + b
- }
+function suma(a, b) {
+  return a + b;
+}
 
- let sum1;
- let sum2;
- let resultado = suma(sum1, sum2)
- console.log(resultado)
+let sum1; //MODIFICA SOLO ESTA LÍNEA
+let sum2; // MODIFICA SOLO ESTA LÍNEA
+let resultado = suma(sum1, sum2);
+console.log(resultado);
 
- // Te has fijado que la función no tiene por que devolver el valor de una variable? Puede devolver cualquier evaluación de una expresión, en este caso, el resultado de evaluar a + b
+// Te has fijado que la función no tiene por que devolver el valor de una variable? Puede devolver cualquier evaluación de una expresión, en este caso, el resultado de evaluar a + b
--- ex3.js
+++ ex3.js
@@ -1,15 +1,15 @@
 /**
  * Hasta ahora hemos visto funciones muy sencillitas, pero pueden ser más complejas.
- * 
+ *
  * La siguiente función escribe 'n' veces el texto que le pasamos en 'texto'
  */
 
- function escribeVeces(n, texto) {
-     for (let i=0; i<n; i++) {
-         console.log(texto)
-     }
- }
+function escribeVeces(n, texto) {
+  for (let i = 0; i < n; i++) {
+    console.log(texto);
+  }
+}
 
- /**
-  * Invoca esta función de manera adecuada para que escribe la palabra 'Hola!' 10 veces
-  */
+/**
+ * Invoca esta función de manera adecuada para que escribe la palabra 'Hola!' 10 veces. ESCRIBE solo a partir de aquí
+ */
