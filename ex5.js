9eff164e59ffbf12390d7ae6ac9785abd28b893a
omiras <oscar.miras@gmail.com>
2025-03-13 17:23:42 +0100

    done

--- ex5.js
+++ ex5.js
@@ -8,6 +8,8 @@ function rollDice() {
  * 
  * En este juego siempre hay que tirar dos dados de 6 y sumar sus resultados.
  * Escribe a continuación el código necesario para invocar la función "rollDice" dos veces, sumar los resultados de las dos tiradas y mostrar el resultado por la consola.
+ * 
+ * Ejemplo de resultado esperado: https://oscarm.tinytake.com/media/173c1e7?filename=1741882982617_TinyTake13-03-2025-05-22-59_638774797817231350.png&sub_type=thumbnail_preview&type=attachment&width=799&height=271
  */
 
 // Tu código a partir de aquí
