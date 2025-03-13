1bf2ac6ff959cbf18659b7bd237eabf00111f799
omiras <oscar.miras@gmail.com>
2025-03-13 21:32:01 +0100

    done ex3b

--- ex3b.js
+++ ex3b.js
@@ -7,7 +7,7 @@ function isPositive(num) {
 }
 
 /**
- * 1. ¿Que nombre tiene esta función?
+ * 1. ¿Que nombre tiene esta función? isPositive
  * 2. ¿Que deberíamos escribir para ejecutar esta función?
  * 3. ¿Qué tipo de datos devuelve esta función? ¿Un string, un number o un boolean?
  * 4. El código de abajo usa la función isPositve. ¿La usa adecuadamente? ¿Por qué?
@@ -15,7 +15,12 @@ function isPositive(num) {
 
 let x = 4;
 if (isPositive(x)) {
-  console.log("El número es negativo");
-} else {
   console.log("El número es positivo");
+} else {
+  console.log("El número es negativo");
 }
+
+// Corregir 21.23
+// 2.
+let resultado1= isPositive(-2);
+// console.log(resultado1);
